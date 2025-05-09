c202b8e1c1bd88b64872dba705e51950495dc10f
AlejandroSierraA <alejandro.sierra.alvarez@zabalburu.org>
2025-05-09 12:58:58 +0200

    Inicio Proyecto

---
+++ Proyecto_recyclon_Java/src/main/java/org/zabalburu/daw1/proyecto_recyclon_java/Proyecto_recyclon_Java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.zabalburu.daw1.proyecto_recyclon_java;
+
+/**
+ *
+ * @author DAW1
+ */
+public class Proyecto_recyclon_Java {
+
+    public static void main(String[] args) {
+        
+    }
+}
