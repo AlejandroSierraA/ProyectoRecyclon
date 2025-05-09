ec5c6c5479f2bfdbc1e1ca92cea91239e026e37b
AlejandroSierraA <alejandro.sierra.alvarez@zabalburu.org>
2025-05-09 13:46:03 +0200

    Inicio Clase

---
+++ Proyecto_recyclon_Java/src/main/java/org/zabalburu/daw1/proyecto_recyclon_java/modelo/Productos.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.zabalburu.daw1.proyecto_recyclon_java.modelo;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author DAW1
+ */
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Setter
+@Getter
+public class Productos {
+    
+}
