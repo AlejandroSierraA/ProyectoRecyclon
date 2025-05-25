f628f992a2ac7bf7fef153e1437656c8293246c2
ImanolMontejo <159002805+ImanolMontejo@users.noreply.github.com>
2025-05-25 11:57:46 +0200

    Corregida llamada
    He arreglado una cosa que le sucedia a la llamada. He nombrado mas la variable declarada y no he puesto unas comuillas. Solucionado

--- llamada procedimiento pedidos.sql
+++ llamada procedimiento pedidos.sql
@@ -5,7 +5,7 @@ set serveroutput on
 DECLARE
 V_ID_USU usuarios.id_usuarios%TYPE;
 BEGIN
-V_ID_usu:=&identificativo_del_usuario;
+V_ID_USU:='&identificativo_del_usuario';
 
 P_CALCULAR_PEDIDOS(V_ID_USU);
 
